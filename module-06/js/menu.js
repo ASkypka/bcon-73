73167af336ea1a980cab159384999bcbed31c255
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-05-31 14:09:26 +0300

    remove event var

--- module-06/js/menu.js
+++ module-06/js/menu.js
@@ -2,7 +2,7 @@ const menuBtns = document.querySelectorAll('.js-menu-btn');
 const menuEl = document.querySelector('.menu-container');
 
 menuBtns.forEach(btn => {
-  btn.addEventListener('click', event => {
+  btn.addEventListener('click', () => {
     menuEl.classList.toggle('is-open');
   });
 });
